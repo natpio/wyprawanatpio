2162dd95e7d998e71af1cf6043d1624007220874
natpio <252204917+natpio@users.noreply.github.com>
2026-04-18 00:22:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -118,20 +118,63 @@ with t4:
             ed_g = st.data_editor(df_g, hide_index=True, num_rows="dynamic")
             if st.button("Zapisz Gry"): st.session_state["df_Grywalizacja"] = ed_g; save_and_sync("Grywalizacja"); safe_rerun()
 
-# --- ZAKŁADKA 5: ODPRAWA (CUSTOMS) ---
+# --- ZAKŁADKA 5: ODPRAWA (CUSTOMS CBP Form 6059B) ---
 with t5:
-    st.info("💡 Wypełnij raz - przepisz w samolocie na niebieski druk!")
+    st.info("💡 Formularz celny wypełnia się **JEDEN na całą rodzinę** (osoby wspólnie zamieszkujące). Wpisz dane poniżej, a po prawej wygeneruje się ściągawka gotowa do szybkiego przepisania w samolocie.")
+    
     c_form, c_card = st.columns([1, 1.2])
     with c_form:
-        c_name = st.text_input("1. Imię i Nazwisko", value=st.session_state.get("c_name", ""))
-        c_dob = st.text_input("2. Data urodzenia (MM/DD/RR)", value=st.session_state.get("c_dob", ""))
-        c_mem = st.number_input("3. Liczba osób z Tobą", min_value=0, value=st.session_state.get("c_mem", 3))
-        c_addr = st.text_input("4. Adres w USA", value=st.session_state.get("c_addr", "Des Moines, IA"))
-        c_pass = st.text_input("5-6. Wydanie i Nr Paszportu", value=st.session_state.get("c_pass", "POLAND, "))
-        c_fly = st.text_input("9. Linia i Nr Lotu", value=st.session_state.get("c_fly", ""))
-        c_gift = st.number_input("15. Wartość prezentów zostawianych w USA ($)", min_value=0, value=st.session_state.get("c_gift", 0))
-        # Zapisujemy do sesji
-        for k, v in zip(["c_name", "c_dob", "c_mem", "c_addr", "c_pass", "c_fly", "c_gift"], [c_name, c_dob, c_mem, c_addr, c_pass, c_fly, c_gift]):
-            st.session_state[k] = v
+        c_last = st.text_input("1. Nazwisko (Family Name)", value=st.session_state.get("c_last", ""))
+        c1a, c1b = st.columns(2)
+        with c1a: c_first = st.text_input("1. Imię (First Name)", value=st.session_state.get("c_first", ""))
+        with c1b: c_middle = st.text_input("1. Drugie imię/Inicjał", value=st.session_state.get("c_middle", ""))
+        
+        c2a, c2b = st.columns(2)
+        with c2a: c_dob = st.text_input("2. Data ur. (MM/DD/RR)", value=st.session_state.get("c_dob", ""))
+        with c2b: c_mem = st.number_input("3. Członkowie rodziny z Tobą", min_value=0, value=st.session_state.get("c_mem", 3))
+        
+        c_street = st.text_input("4a. Ulica i numer / Hotel w USA", value=st.session_state.get("c_street", ""))
+        c4a, c4b = st.columns(2)
+        with c4a: c_city = st.text_input("4b. Miasto w USA", value=st.session_state.get("c_city", "Des Moines"))
+        with c4b: c_state = st.text_input("4c. Stan", value=st.session_state.get("c_state", "IA"))
+        
+        c5a, c5b = st.columns(2)
+        with c5a: c_pass_country = st.text_input("5. Paszport wydany przez", value=st.session_state.get("c_pass_country", "POLAND"))
+        with c5b: c_pass_no = st.text_input("6. Numer paszportu", value=st.session_state.get("c_pass_no", ""))
+        
+        c_residence = st.text_input("7. Państwo zamieszkania", value=st.session_state.get("c_residence", "POLAND"))
+        c_visited = st.text_input("8. Odwiedzone kraje przed przylotem (np. GERMANY)", value=st.session_state.get("c_visited", "NONE"))
+        c_fly = st.text_input("9. Linia lotnicza i nr lotu (Airline/Flight)", value=st.session_state.get("c_fly", ""))
+        
+        st.markdown("---")
+        st.markdown("##### 🛂 Pytania Tak/Nie (Domyślnie NIE):")
+        
+        def r_idx(key): return 1 if "Tak" in st.session_state.get(key, "Nie") else 0
+        
+        c_10 = st.radio("10. Cel podróży to biznes?", ["Nie", "Tak"], index=r_idx("c_10"), horizontal=True)
+        c_11a = st.radio("11a. Wwozisz owoce, warzywa, rośliny, jedzenie?", ["Nie", "Tak"], index=r_idx("c_11a"), horizontal=True)
+        c_11b = st.radio("11b. Wwozisz mięso, zwierzęta, produkty odzwierzęce?", ["Nie", "Tak"], index=r_idx("c_11b"), horizontal=True)
+        c_11c = st.radio("11c. Wwozisz czynniki chorobotwórcze, ślimaki?", ["Nie", "Tak"], index=r_idx("c_11c"), horizontal=True)
+        c_11d = st.radio("11d. Wwozisz ziemię lub byłeś na farmie?", ["Nie", "Tak"], index=r_idx("c_11d"), horizontal=True)
+        c_12 = st.radio("12. Miałeś bliski kontakt z żywym inwentarzem?", ["Nie", "Tak"], index=r_idx("c_12"), horizontal=True)
+        c_13 = st.radio("13. Przewozisz gotówkę powyżej 10 000 USD?", ["Nie", "Tak"], index=r_idx("c_13"), horizontal=True)
+        c_14 = st.radio("14. Przewozisz towary na sprzedaż?", ["Nie", "Tak"], index=r_idx("c_14"), horizontal=True)
+        c_15 = st.number_input("15. Wartość pozostawianych prezentów [$]", min_value=0, value=st.session_state.get("c_15", 0))
+
+        # Automatyczny zapis w pamięci telefonu podczas wpisywania
+        keys = ['c_last','c_first','c_middle','c_dob','c_mem','c_street','c_city','c_state','c_pass_country','c_pass_no','c_residence','c_visited','c_fly','c_10','c_11a','c_11b','c_11c','c_11d','c_12','c_13','c_14','c_15']
+        vals = [c_last,c_first,c_middle,c_dob,c_mem,c_street,c_city,c_state,c_pass_country,c_pass_no,c_residence,c_visited,c_fly,c_10,c_11a,c_11b,c_11c,c_11d,c_12,c_13,c_14,c_15]
+        for k, v in zip(keys, vals): st.session_state[k] = v
+
     with c_card:
-        render_customs_card(c_name, c_dob, c_mem, c_addr, c_pass, c_fly, c_gift)
+        st.markdown("#### 🇺🇸 Gotowa Ściągawka:")
+        def yn(val): return "YES (X)" if "Tak" in val else "NO (X)"
+        
+        dict_data = {
+            'c_last': c_last.upper() if c_last else "", 'c_first': c_first.upper() if c_first else "", 'c_middle': c_middle.upper() if c_middle else "",
+            'c_dob': c_dob, 'c_mem': c_mem, 'c_street': c_street.upper() if c_street else "", 'c_city': c_city.upper() if c_city else "", 'c_state': c_state.upper() if c_state else "",
+            'c_pass_country': c_pass_country.upper() if c_pass_country else "", 'c_pass_no': c_pass_no.upper() if c_pass_no else "",
+            'c_residence': c_residence.upper() if c_residence else "", 'c_visited': c_visited.upper() if c_visited else "", 'c_fly': c_fly.upper() if c_fly else "",
+            'c_10': yn(c_10), 'c_11a': yn(c_11a), 'c_11b': yn(c_11b), 'c_11c': yn(c_11c), 'c_11d': yn(c_11d), 'c_12': yn(c_12), 'c_13': yn(c_13), 'c_14': yn(c_14), 'c_15': c_15
+        }
+        render_customs_card(dict_data)
